95d41badf8f956bacab275823c3387a8eb7b42c7
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-03-24 22:12:11 +0600

    Room Event & Socket.io Essentials + Customer Flow – Part 1 Done.

--- socket/orderHandler.js
+++ socket/orderHandler.js
@@ -6,15 +6,16 @@ export const orderHandler = (io, socket) => {
 
     // EMIT -> TRIGGER -> ON -> LISTEN
 
-    socket.on("placeOrder", async(data, callback) => {
+    // PLACE ORDER
+    socket.on("placeOrder", async (data, callback) => {
         try {
             console.log(`Placed order from ${socket.id}`);
 
             const validation = validateOrder(data);
 
-            if(!validation.valid) {
+            if (!validation.valid) {
                 callback({
-                    success: false, 
+                    success: false,
                     message: validation.message
                 });
             }
@@ -22,28 +23,57 @@ export const orderHandler = (io, socket) => {
             const totals = calculateTotal(data.items);
             const orderId = generateOrderId();
             const order = createOrder(data, orderId, totals);
-            
+
             const ordersCollection = getCollection("orders");
             await ordersCollection.insertOne(order);
 
             socket.join(`order-${orderId}`);
             socket.join(`customers`);
 
-            io.to("admins").emit("newOrder", { 
-                order 
+            io.to("admins").emit("newOrder", {
+                order
             });
 
-            callback({ 
-                success: true, 
-                order 
+            callback({
+                success: true,
+                order
             });
             console.log(`Order created: ${orderId}`)
         } catch (error) {
             console.log(error);
             callback({
-                success: false, 
+                success: false,
                 message: "Failed to place order..."
             })
+        }
+    });
+
+    // TRACK ORDER
+    socket.on("trackOrder", async (data, callback) => {
+        try {
+            const ordersCollection = getCollection("orders");
+            const order = await ordersCollection.findOne({
+                orderId: data, orderId
+            });
+
+            if (!order) {
+                return callback({
+                    success: false,
+                    message: "Order not found"
+                });
+            }
+
+            socket.join(`order-${data.orderId}`);
+            callback({
+                success: true,
+                order
+            });
+        } catch (error) {
+            console.error("Order tracking error", error);
+            callback({
+                success: false,
+                message: error.message
+            });
         }
     })
 };
